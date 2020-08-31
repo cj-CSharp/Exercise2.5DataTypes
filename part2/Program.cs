055c9685b086941966c009a70344bf606f60c5cf
cjconnor18 <cjallen1988@gmail.com>
2020-08-31 14:25:11 -0500

    completed part 2

---
+++ part2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the length of the rectangle?");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("What is the width of the rectangle?");
+            double width = double.Parse(Console.ReadLine());
+            double area = length * width;
+            Console.WriteLine("The area of the rectangle is " + area);
+        }
+    }
+}
